f06df681c8254867da96aa6a1080a9ed24582383
Michele Sciabarra <msciabarra@apache.org>
2019-06-19 19:22:16 +0200

    fix loggings

--- setup.jl
+++ setup.jl
@@ -2,10 +2,9 @@ cd(@__DIR__)
 push!(LOAD_PATH, pwd())
 using Pkg
 pkg"activate ."
-pkg"instantiate"
-pkg"build"
 pkg"add https://github.com/essenciary/Genie.jl"
 if length(ARGS) > 1
     Pkg.add(PackageSpec(url=ARGS[1], rev=ARGS[2]))
-    using KnativeWhisk
 end
+pkg"instantiate"
+pkg"build"
