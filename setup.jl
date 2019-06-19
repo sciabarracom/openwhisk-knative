d67d2378280be92081ef1e2f0a4f1475f516c336
Michele Sciabarra <msciabarra@apache.org>
2019-06-19 18:58:21 +0200

    fixed setup

--- setup.jl
+++ setup.jl
@@ -4,6 +4,7 @@ using Pkg
 pkg"activate ."
 pkg"instantiate"
 pkg"build"
+pkg"add https://github.com/essenciary/Genie.jl"
 if length(ARGS) > 0
     Pkg.add(PackageSpec(url=ARGS[1]))
     using KnativeWhisk
