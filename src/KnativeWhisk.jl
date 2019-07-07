717cdfbfc95b407beeaaf36849c5e80b3c33403b
Michele Sciabarra <msciabarra@apache.org>
2019-07-07 10:13:52 +0200

    install and startup separated

--- src/KnativeWhisk.jl
+++ src/KnativeWhisk.jl
@@ -2,12 +2,13 @@ module KnativeWhisk
 
 include("install.jl")
 include("routes.jl")
-    
-install_knative()
 
 using Genie
 Genie.config.run_as_server = true
 Genie.config.server_host = "0.0.0.0"
-Genie.startup()
+
+export startup
+export install_knative
 
 end # module
+
