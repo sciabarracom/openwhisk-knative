902a79e0bd6b8e3a6ff3bd1bae00c81b3b5a6ce6
Michele Sciabarra <msciabarra@apache.org>
2019-07-16 08:51:35 +0200

    moved installer

--- setup.jl
+++
@@ -1,9 +0,0 @@
-cd(@__DIR__)
-using Pkg
-pkg"add https://github.com/essenciary/Genie.jl"
-if length(ARGS) > 1
-    Pkg.add(PackageSpec(url=ARGS[1], rev=ARGS[2]))
-else
-    push!(LOAD_PATH, "$(pwd())/src")
-end
-pkg"build"
--- src/KnativeWhisk.jl
+++
@@ -1,7 +0,0 @@
-module KnativeWhisk
-
-include("routes.jl")
-using Genie
-Genie.startup(8000, "0.0.0.0"; async=false)
-
-end # module
