a7abbb6a8d224def2fffe810102cf351d8bd36de
Michele Sciabarra <msciabarra@apache.org>
2019-08-15 19:49:21 +0200

    expand yaml

--- controller/kw/kube.go
+++ controller/kw/kube.go
@@ -1,9 +1,15 @@
 package kw
 
 import (
+	"fmt"
+	"strings"
+
 	homedir "github.com/mitchellh/go-homedir"
+	log "github.com/sirupsen/logrus"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/kubernetes/scheme"
 	"k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/clientcmd"
 )
@@ -33,6 +39,35 @@ func NewKube() *Kube {
 	}
 }
 
+func parseK8sYaml(fileAsString string) []runtime.Object {
+
+	//acceptedK8sTypes := regexp.MustCompile(`(Role|ClusterRole|RoleBinding|ClusterRoleBinding|ServiceAccount)`)
+	sepYamlfiles := strings.Split(fileAsString, "---")
+	retVal := make([]runtime.Object, 0, len(sepYamlfiles))
+	for _, f := range sepYamlfiles {
+		if f == "\n" || f == "" {
+			// ignore empty cases
+			continue
+		}
+
+		decode := scheme.Codecs.UniversalDeserializer().Decode
+		obj, groupVersionKind, err := decode([]byte(f), nil, nil)
+
+		if err != nil {
+			log.Println(fmt.Sprintf("Error while decoding YAML object. Err was: %s", err))
+			continue
+		}
+
+		/*if !acceptedK8sTypes.MatchString(groupVersionKind.Kind) {
+			log.Printf("The custom-roles configMap contained K8s object types which are not supported! Skipping object with type: %s", groupVersionKind.Kind)
+		} else {
+		}*/
+		log.Println(groupVersionKind)
+		retVal = append(retVal, obj)
+	}
+	return retVal
+}
+
 // ListNamespaces lists namespaces
 func (k *Kube) ListNamespaces() []string {
 	v1 := k.Client.CoreV1()
@@ -47,6 +82,3 @@ func (k *Kube) ListNamespaces() []string {
 	}
 	return res
 }
-
-
-func
--- controller/kw/kube_test.go
+++ controller/kw/kube_test.go
@@ -6,3 +6,15 @@ func ExampleNewKube() {
 	// Output:
 	// (string) (len=16) "tekton-pipelines"
 }
+
+func ExampleParseK8SYaml() {
+	grep(" (Kind|Name):", parseK8sYaml(`---
+apiVersion: v1
+kind: Namespace
+metadata:
+  name: knative-whisk
+`))
+	// Output:
+	// Kind: (string) (len=9) "Namespace",
+	// Name: (string) (len=13) "knative-whisk",
+}
